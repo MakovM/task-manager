53bb955f81231a4a198e541b28ef254155f2a7c8
MakovM <makovinskiy09@gmail.com>
2025-08-25 17:45:20 +0300

    Add pagination and sidebar

--- tasks/views.py
+++ tasks/views.py
@@ -9,3 +9,19 @@ class TaskListView(generic.ListView):
     context_object_name = "task_list"
     template_name = "tasks/task_list.html"
     paginate_by = 5
+
+    def get_queryset(self):
+        queryset = Task.objects.select_related(
+            "task_type"
+        ).prefetch_related(
+            "assignees",
+            "tags"
+        )
+
+        comp = self.request.GET.get("comp")
+        if comp == "1":
+            queryset = queryset.filter(is_completed=True)
+        elif comp == "0":
+            queryset = queryset.filter(is_completed=False)
+
+        return queryset
