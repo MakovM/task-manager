4c5c2facb1fbd074cf77dfe7849b9dae7535fde5
MakovM <makovinskiy09@gmail.com>
2025-08-31 15:56:57 +0300

    Changed urls to plural

--- tasks/urls.py
+++ tasks/urls.py
@@ -17,13 +17,13 @@ app_name = "tasks"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
-    path("task/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
-    path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("task/<int:pk>/toggle/", TaskToggleView.as_view(), name="task-toggle"),
-    path("task/tags/", TagListView.as_view(), name="tag-list"),
-    path("task/tags/create", TagCreateView.as_view(), name="tag-create"),
-    path("task/tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("task/tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/toggle/", TaskToggleView.as_view(), name="task-toggle"),
+    path("tasks/tags/", TagListView.as_view(), name="tag-list"),
+    path("tasks/tags/create", TagCreateView.as_view(), name="tag-create"),
+    path("tasks/tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tasks/tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
